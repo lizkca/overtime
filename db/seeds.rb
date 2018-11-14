e5a91769290f40c4dd1c3887179e05fa1df58d25
lienxiong <lizkca@163.com>
2018-11-14 11:25:17 +0000

    rending a list of post via bdd and refactoring the index action.(finished chap6-20 and chap6-21)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@user = User.create(email: "test@test.com",
+                 password: "asdfasdf",
+    password_confirmation: "asdfasdf", 
+               first_name: "Jon", 
+                last_name: "Snow")
+puts "1 user created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
