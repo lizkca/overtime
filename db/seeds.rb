7d00d1160d756efad6a20916394ab5c04a601b69
lienxiong <lizkca@163.com>
2018-11-21 07:42:24 +0000

    Customizing Admin Dashboard to Show approval statuses.(finished chap11-39)

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,14 @@
                 last_name: "Snow")
 puts "1 user created"
 
+AdminUser.create(email: "admin@test.com",
+                 password: "asdfasdf",
+    password_confirmation: "asdfasdf", 
+               first_name: "admin", 
+                last_name: "user")
+puts "1 Admin User created"
+
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
